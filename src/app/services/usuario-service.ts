7147cd38e73581598e83c55a0b12f712e8bd53d3
Daniela070806 <naniela070806@gmail.com>
2026-05-21 16:01:01 -0500

    Primer commit

--- src/app/services/usuario-service.ts
+++ src/app/services/usuario-service.ts
@@ -10,7 +10,7 @@ export class UsuarioService {
 
   private http = inject(HttpClient);
 
-  private API_USARIOS = 'https://app-fire-98f40-default-rtdb.firebaseio.com/usuarios.json'
+  private API_USARIOS = 'https://cloudcomputing-ab67e-default-rtdb.firebaseio.com/usuarios.json'
 
   //Método POST
   postUsuarios(usuario:Usuario):Observable<Usuario>{
